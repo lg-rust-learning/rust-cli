da5fb7bcae2fa7b74ba4c2832058992a655f6c6d
Linguan Yang <yanglinguan@gmail.com>
2024-04-21 13:54:19 -0400

    support yaml format

--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::path::Path;
+use std::{fmt, path::Path, str::FromStr};
 
 #[derive(Parser, Debug)]
 #[command(name = "rcli", version, author, about)]
@@ -14,12 +14,20 @@ pub enum SubCommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Parser, Debug)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_file_exists)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
+    #[arg(long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
@@ -31,5 +39,36 @@ fn verify_file_exists(file_name: &str) -> Result<String, String> {
         Ok(file_name.to_string())
     } else {
         Err(format!("File not found: {}", file_name))
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid format: {}", value)),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", Into::<&'static str>::into(*self))
     }
 }
